f78c172c95a641d23665977b0d6f344d676318ab
Joshua Wright <jwright@hasborg.com>
2015-08-06 15:29:09 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,183 @@
+pptxindex
+======
+
+Using a concordance file of keywords or Python expressions, build a Microsoft Word index document from the slides and notes of one or more PowerPoint pptx files.
+
+## Linux or OS X Installation
+
+On Linux or Mac OS X systems, install the `python-pptx` and `python-docx` packages using `pip`:
+
+```
+$ pip install python-docx
+$ pip install python-pptx
+```
+
+## Windows Installation
+
+Windows users can download and run the EXE file included in the `bin/` directory (built with `C:\Python27\scripts\pyinstaller --onefile pptxindex.py`).  Copy this file to a location in your PATH (I copy to `C:\Tools` - YMMV).
+
+
+## Usage
+
+```
+$ pptxindex.py
+Usage: pptxindex.py -c <CONCORDANCE> [-o WORDFILE] [-i WORDFILE] [PPTX FILES]
+                          [-h] [-t]
+     -c <CONCORDANCE>    Specify the concordance filename
+     -o <WORDFILE>       Specify the MS Word index output filename
+     -i <WORDFILE>       Specify the MS Word template file to base index on
+     -t                  Test and validate concordance file syntax, then exit
+     -v                  Verbose output (including 0-hit concordance entries)
+     -h                  This usage information
+```
+
+### Example usage:
+
+Check the concordance file for validity:
+```
+$ pptxindex.py -c concordance.txt -t  
+No errors in the concordance file.
+$ ./pptxindex.py -c concordance-err.txt  -t
+Error processing concordance file line 9: EOL while scanning string literal (<string>, line 1)
+Please correct the errors in the concordance file and try again.
+```
+
+Generate an index for a single PowerPoint file, writing the Word index file to concordance.txt.docx:
+```
+$ ./pptxindex.py -c concordance.txt Sec575_1_A09_RSTR.pptx
+Extracting content from PPTX files.
+Searching for matches with the concordance file.
+Creating index reference ranges.
+Creating index document.
+Done.
+$ ls -l concordance.txt.docx
+-rw-r--r--  1 jwright  staff  41536 Aug  6 08:28 concordance.txt.docx
+```
+
+Generate an index for multiple PowerPoint files, reporting on concordance entries that did not produce matches:
+```
+$ ./pptxindex.py -c concordance.txt -v ~/Documents/Sec575_*.pptx
+Processing PPTX files: Sec575_1_A09_RSTR.pptx Sec575_2_A09_RSTR.pptx Sec575_3_A09_RSTR.pptx Sec575_4_A09_RSTR.pptx Sec575_5_A09_RSTR.pptx Sec575_6_A09_RSTR.pptx Sec575_Handout1.pptx
+Extracting content from PPTX files.
+Searching for matches with the concordance file.
+The following entries in the concordance file did not produce matches:
+    RecoverAndroidPin.py Tool : "recoverandroidpin" in page
+    Recovery Mode
+    iOS Application Folders : "ios" in page and ("application folder" in page or "/Applications" in page)
+Creating index reference ranges.
+Creating index document.
+Done.
+```
+
+Generate a Word index file called "Sec575_A09_Index.docx", using the supplied template file as the first page of the output docx file:
+```
+$ ./pptxindex.py -c concordance.txt -o Sec575_A09_Index.docx -i Template.docx ~/Documents/Sec575_*.pptx
+Extracting content from PPTX files.
+Searching for matches with the concordance file.
+Creating index reference ranges.
+Creating index document.
+Done.
+$ ls -l Sec575_A09_Index.docx
+-rw-r--r--  1 jwright  staff  34634 Aug  6 08:43 Sec575_A09_Index.docx
+```
+
+## Building a Concordance
+
+The concordance file is the file by which you specify the entries that will be used to generate your index.  Your concordance file can be cery simple, matching a list of keywords to use when populating the index document, or it can be complex using Pythonic expressions.
+
+The concordance file is a simple text file, in either Windows or Unix format.  Lines beginning with a "#" are ignored as comments.  Blank lines are also ignored.
+
+### Simple Concordance Entry Matching
+
+Your concordance file can contain a simple list of keywords to use when generating the index.  Each line (ignoring comments and blank lines) in the concordance file is used to produce the entry in the index, and as the search term to match in the PowerPoint files.
+
+```
+$ cat concordance.txt
+Apple Configurator
+iTunes
+Cain & Abel
+Wireshark
+```
+
+Here, each of the 4 entries in the concordance file will be used as both the search terms and the index entry produced in the output Word document.
+
+### Customized Concordance Entry Matching
+
+The concordance file can also use Pythonic expressions to determine if the page of content will produce a hit for a given search term, using a more elegant index entry description than what is actually used for the search term.  Consider the following example:
+
+```
+$ cat concordance.txt
+SQL Injection;"sql injection" in page
+SQL Injection, Testing Risk;"sql injection" in page and "testing risk" in page
+```
+
+Here, two concordance entries are used to index content associated with SQL injection.  The content to the left of the semi-colon is the string used in the index output.  The content to the right of the semi-colon is a Python expression returning a Boolean expression.
+
+The Python expressions in the concordance can be almost anything that is valid Python, referring to one of four internal variables:
+
+1. `page` - The `page` variable contains all the content from an individual page in one or more PowerPoint documents, converted to all lowercase characters.
+2. `cspage` - The `cspage` variable is similar to `page`, except that it is case sentitive.
+3. `pagenum` - The `pagenum` variable is the current page number being evaluated in the PowerPoint file.
+4. `booknum` - The `booknum` variable is the current book number (see "On Page Numbering" below) in the PowerPoint file.
+
+In the first example from the concordance file, the string "SQL Injection" will be added to the index if the Python expression to the right of the semi-colon evaluates to True.  Here, the expression will be True if the string "sql injection" is present in any given PowerPoint page.  This functionality is identical to a concordance entry of simply "SQL Injection" with no expression following a semi-colon.
+
+In the second example from the concordance file, the string "SQL Injection, Testing Risk" will similarly be added to the index if the Python expression to the right of the semi-colon evaluates to True.  Here, the expression will be True if the string "sql injection" is present in any given PowerPoint page, and if "testing risk" is also present in the same page.
+
+### Advanced Concordance Entry Matching
+
+Since the concordance expressions are any valid Python expression, you can be creative in expressing when `pptxindex.py` will return a match for an indexed entry.
+
+```
+$ cat concordance.txt
+Client Side Injection Attacks; ("client side injection" in page or "csi" in page) and "attack" in page
+```
+
+Here, the index entry is "Client Side Injection Attacks", which will trigger anytime the string "attack" is in the page, and when "client side injection" or "csi" is in the page as well.
+
+```
+$ cat concordance.txt
+Burp Suite;page.count("burp suite") > 1
+```
+
+If I feel that the number of page hits are too frequent to be useful in the index (e.g. the indexed list of matches is more than what would be useful as a reference source), then I would consider only registering a match when the number of instances of the search term is greater than 1.  In the previous example, the index entry for "Burp Suite" will only match a page when "Burp Suite" occurs more than once on a page (in the notes of the slide bullets), eliminating most of the casual references to Burp Suite throughout the PowerPoint files.
+
+```
+$ cat concordance.txt
+Penetration Testing, Mobile;("pentest" in page or "pen test" in page or "penetration test" in page) and (booknum == 2 or booknum == 3)
+```
+
+In this example, the index entry for "Penetration Testing, Mobile" will only appear when the strings "pentest", "pen test" or "penetration testing" appear, and the book is the 2nd or 3rd PowerPoint file in the list of files.
+
+
+## On Page Numbering
+
+Pptxindex accepts one or more PowerPoint files to use for building the content to evaluate for indexing. When multiple PowerPoint files are specified, it creates the index using the notation *BookNumber:PageNumber*, or for a range of pages, *BookNumber:StartPageNumber-EndPageNumber* (e.x. "1:6", "4-125", "3:20-23").
+
+The book number is taken from the PowerPoint filename order, alphanumerically sorted.  Consider the following PowerPoint filenames:
+
+```
+$ ls *.pptx
+Sec575_1_A09.pptx
+Sec575_2_A09.pptx
+Sec575_3_A09.pptx
+```
+
+Here, the Sec575_1_A09.pptx will be marked as book number 1 (e.g. "1:6" for hits on page 6).  The file Sec575_2_A09.pptx would be page 2, etc.  This assumes you don't name your files like this:
+
+```
+$ ls *.pptx
+Sec575_MobArch_1_A09.pptx
+Sec575_MobPentest_2_A09.pptx
+Sec575_MobDevRecommend_3_A09.pptx
+```
+
+In this naming convention, the file "Sec575_MobDevRecommend_3_A09.pptx" would be alphanumerically sorted as book 2, and Sec575_MobPentest_2_A09.pptx would be book 3.  *Don't name your files this way.*
+
+
+## Questions, Comments, Concerns?
+
+Open a ticket, or drop me a note: jwright@hasborg.com.
+
+-Josh
+
---
+++ pptxindex.py
@@ -0,0 +1,345 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+#
+
+from pptx import Presentation
+from docx import Document
+from itertools import groupby
+from operator import itemgetter
+from zipfile import ZipFile
+from xml.dom.minidom import parse
+
+import sys
+import re
+import os
+import shutil
+import glob
+import tempfile
+import operator
+import getopt
+import pdb
+import string
+
+def parseslidenotes(pptxfile, words, booknum):
+    tmpd = tempfile.mkdtemp()
+    ZipFile(pptxfile).extractall(path=tmpd, pwd=None)
+    path = tmpd + '/ppt/notesSlides/'
+    for infile in glob.glob(os.path.join(path, '*.xml')):
+        #parse each XML notes file from the notes folder.
+        dom = parse(infile)
+        noteslist = dom.getElementsByTagName('a:t')
+
+        # The page number is part of the filename
+        page = int(re.sub(r'\D', "", infile.split("/")[-1]))
+
+        # Create dictionary entry without content
+        words[str(booknum) + ":" + str(page)] = ""
+        text = ''
+
+        for node in noteslist:
+            xmlTag = node.toxml()
+            xmlData = xmlTag.replace('<a:t>', '').replace('</a:t>', '')
+            #concatenate the xmlData to the text for the particular slideNumber index.
+            text += " " + xmlData
+
+        # Convert to ascii to simplify
+        text = text.encode('ascii', 'ignore')
+        words[str(booknum) + ":" + str(page)] = " " + text
+
+
+    path = tmpd + '/ppt/slides/'
+    for infile in glob.glob(os.path.join(path, '*.xml')):
+        #parse each XML notes file from the notes folder.
+        dom = parse(infile)
+
+        noteslist = dom.getElementsByTagName('a:t')
+
+        page = int(re.sub(r'\D', "", infile.split("/")[-1]))
+        text = ''
+
+        for node in noteslist:
+            xmlTag = node.toxml()
+            xmlData = xmlTag.replace('<a:t>', '').replace('</a:t>', '')
+            text += " " + xmlData
+
+        # Convert to ascii to simplify
+        text = text.encode('ascii', 'ignore')
+        words[str(booknum) + ":" + str(page)] += " " + text
+
+    # Remove all the files created with unzip
+    shutil.rmtree(tmpd)
+
+    # Remove double-spaces which happens in the content occasionally
+    for page in words:
+        words[page] = ''.join(ch for ch in words[page] if ch not in set([',','(',')']))
+        words[page] = re.sub('\. ', " ", words[page])
+        words[page] = ' '.join(words[page].split())
+
+    return words
+
+# Parse the text on slides using the python-pptx module, return words
+def parseslidetext(prs, words, booknum):
+    page = 0
+    for slide in prs.slides:
+        page += 1
+        text_runs = ""
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                for run in paragraph.runs:
+                    try:
+                        text_runs += (" " + run.text)
+                    except TypeError:
+                        continue
+
+        # SEC575 Specific -- skip slides that are course outline slides
+        if text_runs.find("Course") and text_runs.find("Roadmap"): 
+            words[str(booknum)+ ":" + str(page)] = ""
+        else:
+            words[str(booknum)+ ":" + str(page)] = text_runs
+    return words
+
+# Validate the contents of the concordance file
+def checkconcordance(concordancefile):
+    page = ""
+    cspage = ""
+    ret=0
+    lineno=0
+    for line in open(concordancefile):
+        expression = None
+        lineno+=1
+        if line[0] == "#" or line == "\n" or line == "\r\n" or line.isspace(): continue
+        try:
+            key,expression = line.strip().split(";")
+        except ValueError:
+            # Explicit search term, continue
+            continue
+        if expression != None:
+            try:
+                eval(expression)
+            except Exception, e:
+                ret=1
+                sys.stdout.write("Error processing concordance file line " + str(lineno) + ": ")
+                sys.stdout.write(str(e))
+                sys.stdout.write("\n")
+            continue
+    return ret
+
+# Take the index of entries, sort and reduce the page numbers
+# into ranges (e.g. 1:3,1:4,1:5,1:8 into 1:3-5,1:8
+# This is awful and I hope I never have to edit this code.
+def indexreduce(index):
+    for entry in index:
+        matchesbybook = {}
+        pages=index[entry]
+        for bookpage in pages:
+            book,page = bookpage.split(":")
+            page = int(page)
+            try:
+                matchesbybook[book].append(page)
+            except KeyError:
+                matchesbybook[book] = [page]
+        for book in matchesbybook:
+            sortedreduced=[]
+            matchesbybook[book].sort()
+            matchesbybook[book] = numreduce(matchesbybook[book])
+            # Return to 1:66, 2:57 format
+            
+        index[entry] = []
+        for book in matchesbybook:
+            for page in matchesbybook[book]:
+                index[entry].append(book + ":" + page)
+
+    return index
+
+# Take a list of numbers and reduce them into hyphenated ranges for
+# sequential values.
+def numreduce(data):
+    str_list = []
+    for k, g in groupby(enumerate(data), lambda (i,x):i-x):
+       ilist = map(itemgetter(1), g)
+       #print ilist
+       if len(ilist) > 1:
+          str_list.append('%d-%d' % (ilist[0], ilist[-1]))
+       else:
+          str_list.append('%d' % ilist[0])
+    return str_list 
+
+
+def indexsort(string):
+    book,page = string.split(":")
+    page = re.sub('-.*', "", page)
+    return int(book)*(int(page)+1000)
+
+def showconcordancehits(concordancehits, concordance):
+    # Count the number of hits in the concordancehits dictionary
+    # where val != None
+    nohitcount=0
+    for key in concordancehits:
+        if concordancehits[key] == None:
+           nohitcount+=1
+    if nohitcount == 0:
+        print "All entries in the concordance file produced matches."
+        return
+    else:
+        print "The following entries in the concordance file did not produce matches:"
+        for key in concordancehits:
+            if concordancehits[key] == None:
+                if concordance[key] == None:
+                    print "\t" + key
+                else:
+                    print "\t" + key + " : " + concordance[key]
+
+
+def usage(status=0):
+    print "Usage: pptxindex.py -c <CONCORDANCE> [-o WORDFILE] [-i WORDFILE] [PPTX FILES]"
+    print "                          [-h] [-t]"
+    print "     -c <CONCORDANCE>    Specify the concordance filename"
+    print "     -o <WORDFILE>       Specify the MS Word index output filename"
+    print "     -i <WORDFILE>       Specify the MS Word template file to base index on"
+    print "     -t                  Test and validate concordance file syntax, then exit"
+    print "     -v                  Verbose output (including 0-hit concordance entries)"
+    print "     -h                  This usage information"
+    sys.exit(status)
+
+if __name__ == "__main__":
+
+    concordancefile = None
+    indexoutputfile = None
+    testandexit     = None
+    templatefile    = None
+    verbose         = False
+
+    if len(sys.argv) == 1: usage(0)
+
+    opts = getopt.getopt(sys.argv[1:],"i:c:o:htv")
+    
+    for opt,optarg in opts[0]:
+        if opt == "-c":
+            concordancefile = optarg
+        elif opt == "-o":
+            indexoutputfile = optarg
+        elif opt == "-i":
+            templatefile = optarg
+        elif opt == "-t":
+            testandexit = True
+        elif opt == "-v":
+            verbose = True
+        elif opt == "-h":
+            usage()
+    
+    if not concordancefile:
+        print "Error: concordance file not specified"
+        usage()
+
+    if not indexoutputfile:
+        indexoutputfile = concordancefile + ".docx"
+
+    # Check all the expressions in the concordance file
+    if (checkconcordance(concordancefile) != 0):
+        sys.stderr.write("Please correct the errors in the concordance file and try again.\n")
+        sys.exit(-1)
+
+    if testandexit:
+        print("No errors in the concordance file.")
+        sys.exit(0)
+
+    # Read concordance file and build the dictionary
+    concordance = {}
+    for line in open(concordancefile):
+        if line[0] == "#" or line == "^$": continue
+        try:
+            key,val = line.strip().split(";")
+            concordance[key] = val
+        except ValueError:
+            concordance[line.strip()] = None
+    
+
+    # Handle globbing for pptx filenames on Windows
+    pptxfiles = []
+    for filemask in opts[1:][0]:
+        pptxfiles += glob.glob(filemask)
+    if len(pptxfiles) == 0:
+        sys.stderr.write("No matching PPTX files found.\n")
+        sys.exit(1)
+    if verbose:
+        print("Processing PPTX files: %s")%' '.join(os.path.basename(x) for x in pptxfiles)
+
+    print("Extracting content from PPTX files.")
+    wordsbypage = {}
+    booknum=1
+    for pptxfile in pptxfiles:
+        try:
+            prs = Presentation(pptxfile)
+        except Exception:
+            sys.stderr.write("Invalid PPTX file: " + pptxfile + "\n")
+            sys.exit(-1)
+        
+        wordsbypage = parseslidetext(prs,wordsbypage,booknum)
+        try:
+            wordsbypage = parseslidenotes(pptxfile,wordsbypage,booknum)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            sys.exit(-1)
+            
+        booknum+=1
+
+    # Next, iterate through the concordance dictionary, searching for and recording
+    # matches for each entry.
+    print("Searching for matches with the concordance file.")
+    index = {}
+    concordancehits = {}
+    for key in concordance:
+        pages = [] # list of page numbers
+        for bookpagenum in wordsbypage:
+            # To track hits with concordance entries, mark hits for this
+            # entry to None by default.
+            concordancehits[key] = None
+
+            # These are the variables intended to be accessible by the author in the concordance file
+            cspage = wordsbypage[bookpagenum]
+            page = wordsbypage[bookpagenum].lower()
+            booknum,pagenum = bookpagenum.split(":")
+
+            # Process the concordance file entry.  If it is None, then use 
+            # the key as the search string
+            if concordance[key] == None:
+                if (key.lower() in page):
+                        pages.append(bookpagenum)
+            # Else, evaluate the right-side of the concordance entry as a Python expression
+            elif eval(concordance[key]):
+                pages.append(bookpagenum)
+
+            # If the concordance entry generated some matches, add it to the index list
+            if pages != []:
+                index[key] = pages
+                concordancehits[key] = True
+
+    if verbose:
+        showconcordancehits(concordancehits, concordance)
+
+    # Reduce index entries "1:1,1:2,1:3" to 1:1-3"
+    print("Creating index reference ranges.")
+    index = indexreduce(index)
+
+    # Sort the reduced index entries numerically
+    for page in index:
+        index[page] = sorted(index[page], key=indexsort)
+
+    # With index list created, make the Word document
+    print("Creating index document.")
+    document = Document(templatefile)
+    if templatefile != None:
+        document.add_page_break()
+    
+    document.add_heading('Index', level=1)
+    table = document.add_table(rows=0, cols=2, style="Light Shading")
+    for entry in sorted(index.keys(), key=str.lower):
+        row_cells = table.add_row().cells
+        row_cells[0].text = entry
+        row_cells[1].text = ", ".join(index[entry])
+
+    document.save(indexoutputfile)
+    print("Done.")
